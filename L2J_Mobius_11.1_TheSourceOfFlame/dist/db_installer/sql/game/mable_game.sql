06d94af2e49da69e67faa70b577a676e621fbfb3
MobiusDevelopment <mobius@cyber-wizard.com>
2025-01-31 17:35:31 +0200

    Removal of L2J_Mobius_11.1_TheSourceOfFlame.

--- L2J_Mobius_11.1_TheSourceOfFlame/dist/db_installer/sql/game/mable_game.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS `mable_game`;
-CREATE TABLE `mable_game` (
-  `account_name` varchar(255) NOT NULL,
-  `round` int(11) NOT NULL,
-  `current_cell_id` int(11) NOT NULL,
-  `remain_common_dice` int(11) NOT NULL,
-  `remain_prison_rolls` int(11) NOT NULL,
-  PRIMARY KEY (`account_name`)
-) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
